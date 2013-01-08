020b364c1d91eb063cc9b150a2eeb6397ae2d433
fifty <Fifty@patch.com>
2013-01-07 21:33:33 -0500

    initial ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColorSwap::Application.config.secret_token = '6ebfe667343eae7250b6276f71abcc28e3d353f94cd3e84b377471b7f13b584efafbf53dc493c9c028e1d36f15634309d45c9f57ea2f5d8d3dfc06dd311cbd57'
